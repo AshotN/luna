7f15ae444a3e95b65b3a9f4317984f760276b732
Ashot Nazaryan <contact@hego.co>
2019-01-13 23:25:01 -0800

    Thieving on Man, Woman, and Guards

---
+++ plugins/world/player/skills/thieving/NPC.kt
@@ -0,0 +1,39 @@
+package world.player.skills.thieving
+
+import api.predef.*
+
+class StealTableItem(val itemId: Int,
+                      val itemQuantity: IntRange,
+                      val levelRequired: Int,
+                      val chance: Int)
+
+/**
+ * An enum representing NPCs that can be pickpocketed.
+ */
+enum class NPC(val id: List<Int>,
+                val level: Int,
+                val stealTable: List<StealTableItem>,
+                val formattedName: String) {
+
+    MAN(id = listOf(1,2,3),
+            level = 1,
+            stealTable = listOf(StealTableItem(995, 1..1, 1, 100)),
+            formattedName = "Man"),
+    WOMAN(id = listOf(4,5,6),
+            level = 1,
+            stealTable = listOf(StealTableItem(995, 1..1, 1, 100)),
+            formattedName = "Woman"),
+    FARMER(id = listOf(7),
+            level = 10,
+            stealTable = listOf(StealTableItem(995, 9..9, 10, 90), StealTableItem(5318,1..1, 1, 10)),
+            formattedName = "Farmer"),
+    GUARD(id = listOf(9,10,32),
+            level = 40,
+            stealTable = listOf(StealTableItem(995, 30..30, 40, 100)),
+            formattedName = "Guard");
+
+
+    companion object {
+        fun from(i: Int): NPC? = values().find { it.id.contains(i) }
+    }
+}
---
+++ plugins/world/player/skills/thieving/Plunder.kt
@@ -0,0 +1,14 @@
+package world.player.skills.thieving
+
+/**
+ * An enum representing a items that can be stolen.
+ */
+enum class Plunder(val id: Int,
+                   val level: Int) {
+
+
+    BOTTLEOFWINE(id = 7919,
+            level = 22),
+    JUGOFWINE(id = 1993,
+            level = 22);
+}
